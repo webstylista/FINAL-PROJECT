928a958ed0284322ac8344f3f23f75082ab42ed5
webstylista <tash.haron@gmail.com>
2017-07-27 18:14:03 +1000

    Version 2.1

--- script/main.js
+++ script/main.js
@@ -1,72 +1,34 @@
 $(document).ready(function() {
 
-  //show '#about' modal
-  $('.open-about').click(function() {
-    $('#about').slideDown(800);
+  //open modals
+  function openModal(event) {
+    var formId = $(event.target).attr('data-form');
+    $('#' + formId).slideDown(800);
     $('#header').fadeOut(600);
     $('footer').hide();
-  });
+  }
 
-  //close '#about' modal
+  //show modal
+  $('.open').click(openModal);
+
+  //close modals
   $('.close').click(function() {
-    $('#about').slideUp(800);
+    $(this).parent().slideUp(800);
     $('#header').fadeIn(600);
     $('footer').show();
-  });
-
-  //show '#soaps' modal
-  $('.open-soaps').click(function() {
-    $('#soaps').slideDown(800);
-    $('#header').fadeOut(600);
-    $('footer').hide();
-  });
-
-  //close '#soaps' modal
-  $('.close').click(function() {
-    $('#soaps').slideUp(800);
-    $('#header').fadeIn(600);
-    $('footer').show();
-  });
-
-  //show '#shop' modal
-  $('.open-shop').click(function() {
-    $('#shop').slideDown(800);
-    $('#header').fadeOut(600);
-    $('footer').hide();
-  });
-
-  //close '#shop' modal
-  $('.close').click(function() {
-    $('#shop').slideUp(800);
-    $('#header').fadeIn(600);
-    $('footer').show();
-  });
-
-  //show '#enquire' modal
-  $('.open-enquire').click(function() {
-    $('#enquire').slideDown(800);
-    $('#header').fadeOut(600);
-    $('footer').hide();
   });
 
   //show 'thank you' message on form submit in '#enquire' modal
   $('.show-thanks').click(function() {
 
       var thanks = function(event) {
-      event.preventDefault();
-      $('#enquire h2').text("Thank you for your enquiry.")
-      $('form').hide();
-      $('#enquire p').hide();
+        event.preventDefault();
+        $('#enquire h2').text("Thank you for your enquiry.")
+        $('form').hide();
+        $('#enquire p').hide();
       }
 
       $('form').submit(thanks);
-  });
-
-  //close '#enquire' modal
-  $('.close').click(function() {
-    $('#enquire').slideUp(800);
-    $('#header').fadeIn(600);
-    $('footer').show();
   });
 
 });
